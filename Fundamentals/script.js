1851ea1884d64502aab42795656c43b469823443
John-P-Foster <johnpete.foster@gmail.com>
2025-07-17 11:53:01 -0400

    Added notes on data types

--- Fundamentals/script.js
+++ Fundamentals/script.js
@@ -1,3 +1,41 @@
 let js = "amazing";
 let result = 40 + 8 + 23 - 10;
 console.log(result);
+
+let firstName = "Slooow Guy"
+
+console.log(firstName); 
+
+// Data types in JavaScript
+/*
+   1. Number: Floating point numbers used for decimal and integers
+   2. String: Sequence of characters.  
+   3. Boolean: Logical type that can only be true or false.valueOf
+   4. Undefined: Value take by a variable that is not yet defined ('empty value').
+   5. Null: Also means 'empty value'.
+   6. BigInt(ES2020): Larger integers than the Number type can support. 
+ */
+
+/*
+    JavaScript has dynamic typing, the data types are determined on the fly. 
+
+*/
+
+
+console.log(typeof true); 
+
+/**
+ * Const vs let vs var
+ * 
+ * let : variable can be mutated at will; 
+ * 
+ * const: creates an immutable variable. 
+ * 
+ * var: legacy way of writing let. 
+ */
+
+// Basix Operators
+ let now = 2025
+ const ageJohn = now - 1991; 
+ const ageMia = now - 1994;
+ console.log(ageJohn, ageMia); 
