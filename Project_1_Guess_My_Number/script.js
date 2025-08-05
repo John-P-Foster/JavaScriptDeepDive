b31555faa4c07003bd771ddb41f148cb8916a7d4
John-P-Foster <johnpete.foster@gmail.com>
2025-08-05 19:31:17 -0400

    Updated guess my number logic

--- Project_1_Guess_My_Number/script.js
+++ Project_1_Guess_My_Number/script.js
@@ -20,11 +20,39 @@ document.querySelector('.guess').value = 23;
 
 
 // the addEventListener expects to params, the event and a function. 
+
+const secretNumber = Math.floor(Math.random() * 21);
+const domSecretNumber = document.querySelector('.number');
+domSecretNumber.textContent = secretNumber; 
+
+let score = 20; 
+const domScore = document.querySelector('.score'); 
+domScore.textContent = score; 
+
+const domPlayerMessage = document.querySelector('.message');
+
+
+// while(number != 20){
+//     number = Math.floor(Math.random() * 21)
+//     console.log(number); 
+// }
+
+
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value); 
 
     if(!guess){
-        document.querySelector('.message').textContent = ' ❌ No Number Given!'
+        domPlayerMessage.textContent = ' ❌ No Number Given!'
+    }else if(guess === secretNumber){
+        domPlayerMessage.textContent = '🎉 Correct Number!'   
+    }else if(guess > secretNumber){
+        domPlayerMessage.textContent = '🔥 Guess is Too high'
+        score --
+        domScore.textContent = score
+    }else{
+        domPlayerMessage.textContent = '❄️ Guess is Too low'
+        score --
+        domScore.textContent = score
     }
 
 })
