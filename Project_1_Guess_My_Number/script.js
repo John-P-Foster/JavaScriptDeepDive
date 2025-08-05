d3cff5688f040d0b4f57342116d5b926ee934630
John-P-Foster <johnpete.foster@gmail.com>
2025-08-05 14:09:34 -0400

    Working on DOM manipulation

--- Project_1_Guess_My_Number/script.js
+++ Project_1_Guess_My_Number/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+/*
+                  Notes section
+    DOM: Document Object Model 
+    - How JavaScript breaks up HTML elements to manipulate them. 
+    - The DOM methods and properties are acutally part of WEB APIs NOT JS. 
+*/
+
+/* 
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number!'
+
+document.querySelector('.number').textContent = 13; 
+document.querySelector('.score').textContent = 20; 
+
+document.querySelector('.guess').value = 23; 
+*/
+
+
+// the addEventListener expects to params, the event and a function. 
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value); 
+
+    if(!guess){
+        document.querySelector('.message').textContent = ' ❌ No Number Given!'
+    }
+
+})
