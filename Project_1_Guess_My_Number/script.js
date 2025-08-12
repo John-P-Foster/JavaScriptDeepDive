cf95fd34dc7e5be8ba7aaf8feeac13229d37127a
John-P-Foster <johnpete.foster@gmail.com>
2025-08-12 14:46:57 -0400

    Finished Guess My Number game.

--- Project_1_Guess_My_Number/script.js
+++ Project_1_Guess_My_Number/script.js
@@ -21,15 +21,20 @@ document.querySelector('.guess').value = 23;
 
 // the addEventListener expects to params, the event and a function. 
 
-const secretNumber = Math.floor(Math.random() * 21);
+const newSecretNumber = function() {return Math.floor(Math.random() * 20) + 1};
+let secretNumber = newSecretNumber();
 const domSecretNumber = document.querySelector('.number');
-domSecretNumber.textContent = secretNumber; 
 
 let score = 20; 
 const domScore = document.querySelector('.score'); 
 domScore.textContent = score; 
 
 const domPlayerMessage = document.querySelector('.message');
+
+const domGuess = document.querySelector('.guess');
+
+let highScore = 0; 
+const domHighScore = document.querySelector('.highscore');
 
 
 // while(number != 20){
@@ -41,18 +46,38 @@ const domPlayerMessage = document.querySelector('.message');
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value); 
 
+    // No input
     if(!guess){
         domPlayerMessage.textContent = ' ❌ No Number Given!'
+    
+    // Got the correct number.     
     }else if(guess === secretNumber){
-        domPlayerMessage.textContent = '🎉 Correct Number!'   
-    }else if(guess > secretNumber){
-        domPlayerMessage.textContent = '🔥 Guess is Too high'
-        score --
-        domScore.textContent = score
-    }else{
-        domPlayerMessage.textContent = '❄️ Guess is Too low'
-        score --
+        domPlayerMessage.textContent = '🎉 Correct Number!' ;
+        domSecretNumber.textContent = secretNumber; 
+        document.querySelector('body').style.backgroundColor = '#60b347';  
+        document.querySelector('.number').style.width='30rem';
+        highScore = Math.max(score, highScore); 
+        domHighScore.textContent = highScore; 
+
+    // Check if guess was higher or lower than secret number. 
+    }else {
+        domPlayerMessage.textContent = guess > secretNumber ? '🔥 Guess is Too high' : '❄️ Guess is Too low';
+        score --; 
         domScore.textContent = score
     }
+
+})
+
+
+// Reset the game for the next round 
+document.querySelector('.again').addEventListener('click', function() {
+    secretNumber = newSecretNumber(); 
+    score = 20; 
+    domSecretNumber.textContent = '?'; 
+    domScore.textContent = score;
+    domGuess.value = ''
+    domPlayerMessage.textContent = 'Start guessing...'
+    document.querySelector('body').style.backgroundColor = '#222';  
+    document.querySelector('.number').style.width='15rem';
 
 })
