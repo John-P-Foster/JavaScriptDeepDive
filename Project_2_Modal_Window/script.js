23cb6f44934400a63373545d5bbd8a2d3a929b8a
John-P-Foster <johnpete.foster@gmail.com>
2025-08-12 15:33:10 -0400

    Finished Models project.

--- Project_2_Modal_Window/script.js
+++ Project_2_Modal_Window/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+
+// Dom Selectors
+const modal = document.querySelector('.modal'); 
+const overlay = document.querySelector('.overlay'); 
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal'); 
+
+// Functions
+const openModel = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden'); 
+}
+
+const closeModel  = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden'); 
+}
+
+
+// Logic
+for(let btn of btnOpenModal){
+    btn.addEventListener('click', openModel)
+}
+
+btnCloseModal.addEventListener('click', closeModel);
+overlay.addEventListener('click', closeModel); 
+
+document.addEventListener('keydown', function(e){
+    const key = e.key; 
+    console.log(key)
+    if(key === 'Escape' && !modal.classList.contains('hidden')){closeModel()}; 
+})
