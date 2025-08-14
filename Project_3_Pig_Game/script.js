52b6a33244b96fb39a8947606e867d8e315b0678
John-P-Foster <johnpete.foster@gmail.com>
2025-08-13 22:11:18 -0400

    Finished dice game.

--- Project_3_Pig_Game/script.js
+++ Project_3_Pig_Game/script.js
@@ -5,27 +5,49 @@ const score0Dom = document.querySelector('#score--0');
 const current0Dom = document.getElementById('current--0') 
 const score1Dom = document.getElementById('score--1');
 const diceDom = document.querySelector('.dice');
-const rollDiceDom = document.querySelector('.btn--roll');
+const rollDiceDom = document.getElementById('btn--roll');
+const holdbuttonDom = document.getElementById('btn--hold');
+const newGameButtonDom = document.getElementById('btn--new');
+const player0Dom = document.querySelector('.player--0');
+const player1Dom = document.querySelector('.player--1');
+
 
 // Variables
-let score0 = 0; 
-let score1 = 0; 
+const scores = [0, 0]
 let currentScore = 0; 
+let activePlayer = 0; 
+
 
 // Button Listeners
 rollDiceDom.addEventListener('click', rollDice); 
+holdbuttonDom.addEventListener('click', holdScore); 
+newGameButtonDom.addEventListener('click', newGame);
 
-// Setting elements
-score0Dom.textContent = score0; 
-score1Dom.textContent = score1;
+// Setting start conditions
+score0Dom.textContent = scores[0]; 
+score1Dom.textContent = scores[1];
+// diceDom.classList.toggle('hidden'); 
+newGameButtonDom.classList.toggle('hidden');
 
 
-diceDom.classList.add('hidden'); 
 
+// Helper functions
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function changePlayer(){
+    // Reset current score. 
+    currentScore = 0; 
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore; 
+
+    // Change players.
+    activePlayer = activePlayer === 0 ? 1 : 0; 
+    player0Dom.classList.toggle('player--active');
+    player1Dom.classList.toggle('player--active')
+}
+
+// Button functions
 async function rollDice(){
     diceDom.classList.remove('hidden')
     let diceValue = 0;
@@ -36,7 +58,42 @@ async function rollDice(){
         diceDom.src = `dice-${diceValue}.png`; 
     }
 
-    currentScore = diceValue === 1 ? 0: currentScore + diceValue; 
-    current0Dom.textContent = currentScore; 
+    if(diceValue != 1){
+        currentScore += diceValue;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore; 
+    }else{
+        changePlayer(); 
+    }
+}
 
+function holdScore(){
+
+    // Update active player running score.
+    scores[activePlayer] += currentScore; 
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+    // Check for winner or switch.
+    if(scores[activePlayer] >= 100){
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+        rollDiceDom.classList.toggle('hidden');
+        holdbuttonDom.classList.toggle('hidden'); 
+        newGameButtonDom.classList.toggle('hidden');
+    }else{
+        changePlayer();
+    }
+}
+
+function newGame(){
+    currentScore = 0; 
+    for(let i = 0; i < 2; i ++){
+        scores[i] = 0; 
+        document.getElementById(`score--${i}`).textContent = scores[i];
+        document.getElementById(`current--${i}`).textContent = currentScore; 
+        document.querySelector(`.player--${i}`).classList.remove('player--winner');
+    }
+    document.querySelector(`.player--0`).classList.add('player--active');
+    rollDiceDom.classList.toggle('hidden');
+    holdbuttonDom.classList.toggle('hidden'); 
+    newGameButtonDom.classList.toggle('hidden');
 }
